c346c9e92ceefc6d854e9e5aebb05f079074687b
Alexandre Aubin <alexandre.aubin@cern.ch>
2018-04-02 19:15:23 +0200

    Some cleaning after the install of yunohost

--- stage3/00-install-yunohost/00-run.sh
+++ stage3/00-install-yunohost/00-run.sh
@@ -4,5 +4,8 @@ on_chroot << EOF
 cd /tmp/
 wget -O install_yunohost https://install.yunohost.org/stretch
 chmod +x /tmp/install_yunohost
-./install_yunohost -a -i
+./install_yunohost -a
+rm -f /etc/ssh/ssh_host_*
+apt-get clean
+find /var/log -type f -exec rm {} \;
 EOF
