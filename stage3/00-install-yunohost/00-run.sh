0204dc079308b227aa32fa5c7b36cecd02b7747d
Alexandre Aubin <alexandre.aubin@cern.ch>
2018-04-03 16:02:23 +0200

    Cleaning yunohost installation stage + install insserv

--- stage3/00-install-yunohost/00-run.sh
+++ stage3/00-install-yunohost/00-run.sh
@@ -1,15 +1,24 @@
 #!/bin/bash -e
 
 on_chroot << EOF
+apt-get install insserv -y
 cd /tmp/
 wget -O install_yunohost https://install.yunohost.org/stretch
 chmod +x /tmp/install_yunohost
 ./install_yunohost -a
 rm -f /etc/ssh/ssh_host_*
-apt-get clean
-find /var/log -type f -exec rm {} \;
+EOF
+
+echo "Enabling ssh login for root + setting default password"
+on_chroot << EOF
 touch /boot/ssh
 sed -i '/PermitRootLogin/c\PermitRootLogin yes' /etc/ssh/sshd_config
 echo "root:yunohost" | chpasswd
 chage -d 0 root
+EOF
+
+echo "Cleaning ..."
+on_chroot << EOF
+apt-get clean
+find /var/log -type f -exec rm {} \;
 EOF
